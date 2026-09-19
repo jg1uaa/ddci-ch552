d634fb4016117241ac0545df0721e2810fa36ad4
SASANO Takayoshi <uaa@uaa.org.uk>
2026-09-19 21:00:10 +0900

    working code

--- ddci-ch552.ino
+++ ddci-ch552.ino
@@ -1,89 +1,124 @@
-/*
-  HID Keyboard example
-
-
-  created 2020
-  by Deqing Sun for use with CH55xduino
-
-  This example code is in the public domain.
-
-  cli board options: usb_settings=user148
-
-*/
-
-//For windows user, if you ever played with other HID device with the same PID C55D
-//You may need to uninstall the previous driver completely        
-
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2026 SASANO Takayoshi <uaa@uaa.org.uk>
 
 #ifndef USER_USB_RAM
-#error "This example needs to be compiled with a USER USB setting"
+#error "USB Settings: USER CODE w/ 148B USB ram required"
+#endif
+
+#if (F_CPU % 12000)
+#error "CPU clock must be (12000 * n) Hz"
 #endif
 
 #include "src/userUsbHidKeyboard/USBHIDKeyboard.h"
 
-#define BUTTON1_PIN 30
-#define BUTTON2_PIN 31
-#define BUTTON3_PIN 32
+#define PIN0_ON 0x01
+#define PIN1_ON 0x02
+#define PIN_MASK (PIN0_ON | PIN1_ON)
 
-#define LED_BUILTIN 33
+#define LED_pin 30
+#define LED_port P3_0
 
-bool button1PressPrev = false;
-bool button2PressPrev = false;
-bool button3PressPrev = false;
+#define PIN0_pin 33
+#define PIN0_port P3_3
 
+#define PIN1_pin 34
+#define PIN1_port P3_4
 
-void setup() {
-  USBInit();
-  pinMode(BUTTON1_PIN, INPUT_PULLUP);
-  pinMode(BUTTON2_PIN, INPUT_PULLUP);
-  pinMode(BUTTON3_PIN, INPUT_PULLUP);
-  pinMode(LED_BUILTIN, OUTPUT);
+volatile unsigned char TimerExpired = 0;
+volatile unsigned char CurrPinStatus = 0;
+volatile unsigned char PrevPinStatus = 0;
+#define PinMaskCount 4 // 4ms debounce
+volatile unsigned char PinMaskCounter0 = 0;
+volatile unsigned char PinMaskCounter1 = 0;
+
+/* due to limitation of library, no Ctrl key support */
+#define PIN0_KEY '['
+#define PIN1_KEY ']'
+
+static void update_pin_status(void)
+{
+	CurrPinStatus = 0;
+
+	if (!PIN0_port) CurrPinStatus |= PIN0_ON;
+	if (!PIN1_port) CurrPinStatus |= PIN1_ON;
+
+	LED_port = CurrPinStatus ? 1 : 0;
 }
 
-void loop() {
-  //button 1 is mapped to letter 'a'
-  bool button1Press = !digitalRead(BUTTON1_PIN);
-  if (button1PressPrev != button1Press) {
-    button1PressPrev = button1Press;
-    if (button1Press) {
-      Keyboard_press('a');
-    } else {
-      Keyboard_release('a');
-    }
-  }
+void Timer2Interrupt(void) __interrupt(INT_NO_TMR2)
+{
+	TF2 = 0; // clear timer flag
 
-  //button 2 is mapped to string 'hello'
-  bool button2Press = !digitalRead(BUTTON2_PIN);
-  if (button2PressPrev != button2Press) {
-    button2PressPrev = button2Press;
-    if (button2Press) {
-      Keyboard_write('H');
-      Keyboard_write('e');
-      Keyboard_write('l');
-      Keyboard_write('l');
-      Keyboard_write('o');
-    }
-  }
+	update_pin_status();
 
-  //button 3 is mapped to Capslock
-  bool button3Press = !digitalRead(BUTTON3_PIN);
-  if (button3PressPrev != button3Press) {
-    button3PressPrev = button3Press;
-    if (button3Press) {
-      Keyboard_press(KEY_CAPS_LOCK);
-      delay(100); //a quick capslock press is not recognized on mac
-      Keyboard_release(KEY_CAPS_LOCK);
-    }
-  }
+	if (PinMaskCounter0) PinMaskCounter0--;
+	if (PinMaskCounter1) PinMaskCounter1--;
 
-  //map capsLock to LED
-  //Bit 0: NUM lock, Bit 1: CAPS lock, Bit 2: SCROLL lock, Bit 3: Compose, Bit 4: Kana,
-  if (Keyboard_getLEDStatus() & 2) {
-    digitalWrite(LED_BUILTIN, HIGH);
-  } else {
-    digitalWrite(LED_BUILTIN, LOW);
-  }
+	TimerExpired = 1;
+}
 
-  delay(50);  //naive debouncing
+void timer_init(void)
+{
+	unsigned short limit;
 
+	/* set timer mode (Fsys/12) */
+	T2CON = 0x00;
+	T2MOD &= ~(bT2_CLK | bT2_CAP_M1 | bT2_CAP_M0 | T2OE | bT2_CAP1_EN);
+
+	/* set interrupt cycle (1ms) */
+	limit = 65536U - (F_CPU / 12000U);
+
+	RCAP2H = TH2 = limit >> 8;
+	RCAP2L = TL2 = limit;
+
+	/* enable interrupt */
+	ET2 = 1; // enable Timer2 interrupt
+	TR2 = 1; // start Timer2
+}
+
+void setup(void)
+{
+	pinMode(PIN0_pin, INPUT_PULLUP);
+	pinMode(PIN1_pin, INPUT_PULLUP);
+	pinMode(LED_pin, OUTPUT);
+
+	timer_init();
+	USBInit();
+}
+
+void loop(void)
+{
+	unsigned char changed, useprev;
+	signed char pin0, pin1;
+
+	while (!TimerExpired);
+
+	EA = 0; // disable interrupt
+
+	changed = (CurrPinStatus ^ PrevPinStatus) & PIN_MASK;
+	useprev = ((PinMaskCounter0 ? PIN0_ON : 0) |
+		   (PinMaskCounter1 ? PIN1_ON : 0));
+	pin0 = pin1 = 0;
+
+	if (changed ^ useprev) {
+		PrevPinStatus =
+			(PrevPinStatus & useprev) | (CurrPinStatus & ~useprev);
+		if (changed & ~useprev & PIN0_ON) {
+			PinMaskCounter0 = PinMaskCount;
+			pin0 = (CurrPinStatus & PIN0_ON) ? 1 : -1;
+		}
+		if (changed & ~useprev & PIN1_ON) {
+			PinMaskCounter1 = PinMaskCount;
+			pin1 = (CurrPinStatus & PIN1_ON) ? 1 : -1;
+		}
+	}
+	TimerExpired = 0;
+
+	EA = 1; // enable interupt
+
+	if (pin0 < 0) Keyboard_release(PIN0_KEY);
+	else if (pin0 > 0) Keyboard_press(PIN0_KEY);
+		
+	if (pin1 < 0) Keyboard_release(PIN1_KEY);
+	else if (pin1 > 0) Keyboard_press(PIN1_KEY);
 }
