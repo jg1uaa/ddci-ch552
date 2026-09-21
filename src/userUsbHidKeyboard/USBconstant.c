f0fca8da7db9ceefd2de726e356f727c87f0f41c
SASANO Takayoshi <uaa@uaa.org.uk>
2026-09-21 16:35:01 +0900

    USBconstant.c: bInterval 10 -> 1

--- src/userUsbHidKeyboard/USBconstant.c
+++ src/userUsbHidKeyboard/USBconstant.c
@@ -75,7 +75,7 @@ __code USB_Descriptor_Configuration_t ConfigurationDescriptor = {
                                  (EP_TYPE_INTERRUPT | ENDPOINT_ATTR_NO_SYNC |
                                   ENDPOINT_USAGE_DATA),
                              .EndpointSize = KEYBOARD_EPSIZE,
-                             .PollingIntervalMS = 10},
+                             .PollingIntervalMS = 1},
 
     /*.HID_ReportOUTEndpoint = {.Header = {.Size =
                                              sizeof(USB_Descriptor_Endpoint_t),
@@ -86,7 +86,7 @@ __code USB_Descriptor_Configuration_t ConfigurationDescriptor = {
                                   (EP_TYPE_INTERRUPT | ENDPOINT_ATTR_NO_SYNC |
                                    ENDPOINT_USAGE_DATA),
                               .EndpointSize = KEYBOARD_EPSIZE,
-                              .PollingIntervalMS = 10},*/
+                              .PollingIntervalMS = 1},*/
 };
 
 __code uint8_t ReportDescriptor[] = {
